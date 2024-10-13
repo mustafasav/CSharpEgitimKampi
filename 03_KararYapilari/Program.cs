3858be5af46cc40187f487d809990d2a45aebfd9
mustafasav <mustafasav@gmail.com>
2024-10-13 18:13:58 +0300

    .

--- 03_KararYapilari/Program.cs
+++ 03_KararYapilari/Program.cs
@@ -57,23 +57,36 @@ namespace _03_KararYapilari
             //else if (enterNumber < 0) { Console.WriteLine("Girilen sayı negatif"); }
             //else { Console.WriteLine("Girilen sayı pozitif"); }
 
-            double exam1, exam2, exam3, result;
-            Console.Write("1. Sınav Notu: ");
-            exam1 = double.Parse(Console.ReadLine());
-            Console.Write("2. Sınav Notu: ");
-            exam2 = double.Parse(Console.ReadLine());
-            Console.Write("3. Sınav Notu: ");
-            exam3 = double.Parse(Console.ReadLine());
+            //  double exam1, exam2, exam3, result;
+            //  Console.Write("1. Sınav Notu: ");
+            //  exam1 = double.Parse(Console.ReadLine());
+            //  Console.Write("2. Sınav Notu: ");
+            //  exam2 = double.Parse(Console.ReadLine());
+            //  Console.Write("3. Sınav Notu: ");
+            //  exam3 = double.Parse(Console.ReadLine());
 
-            result = (exam1 + exam2 + exam3) / 3;
+            //  result = (exam1 + exam2 + exam3) / 3;
 
-            Console.WriteLine("Ortalamanız: "+result);
+            //  Console.WriteLine("Ortalamanız: "+result);
 
-            if(result<=45) { Console.WriteLine("Vasat"); }
-          else  if (result <= 70) { Console.WriteLine("İdare Eder"); }
-            else if (result <= 85) { Console.WriteLine("İyi"); }
-            else { Console.WriteLine("Başarılı"); }
-            
+            //  if(result<=45) { Console.WriteLine("Vasat"); }
+            //else  if (result <= 70) { Console.WriteLine("İdare Eder"); }
+            //  else if (result <= 85) { Console.WriteLine("İyi"); }
+            //  else { Console.WriteLine("Başarılı"); }
+
+            string city;
+            Console.WriteLine("Lütfen şehir giriniz:");
+            city = Console.ReadLine();
+
+            if (city == "adana" | city == "ankara" | city == "bursa" | city == "trabzon")
+            {
+                Console.WriteLine("Şehir mevcut");
+
+            }
+            else
+            {
+                Console.WriteLine("Şehir Mevcut Değil");
+            }
             #endregion
 
             Console.Read();
