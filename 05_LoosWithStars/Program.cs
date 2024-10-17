54d2d005c23339486a9c8266575839c95d9ce5e9
mustafasav <mustafasav@gmail.com>
2024-10-17 15:55:58 +0300

    .

---
+++ 05_LoosWithStars/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_LoosWithStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region alt_alta_10_yildiz_olusturma
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+            #endregion
+
+            #region yanyana_10_yildiz_olusturma
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    Console.Write("*");
+            //}
+            #endregion
+
+            #region alt_alta_10_yildiz_olusturma_hersatir_10_yildiz
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    for(int j = 0; j <= 10; j++)
+            //    {
+            //        Console.Write("* ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            #region yildizlarla_dik_ucgen_
+
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    for (int j = 0; j < i; j++)
+            //    {
+            //        Console.Write("* ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.WriteLine("--------------------");
+            #endregion
+
+            #region yildizlarla_ters_dik_ucgen_
+
+            //for (int i = 10; i >= 0; i--)
+            //{
+            //    for (int j = 0; j < i; j++)
+            //    {
+            //        Console.Write("* ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.WriteLine("--------------------");
+            #endregion
+
+            #region yildizlarla_ters_ters_dik_ucgen_
+
+            //for (int i = 10; i >= 0; i--)
+            //{
+            //    for (int j = 0; j <= (10 - i); j++)
+            //    {
+            //        Console.Write("  ");
+            //    }
+            //    for (int j = 10; j >= (10 - i); j--)
+            //    {
+            //        Console.Write("* ");
+            //    }
+            //    Console.WriteLine();
+
+            //}
+            #endregion
+
+
+            #region elmas_gorseli
+
+
+            #endregion
+            Console.Read();
+
+        }
+    }
+}
